496a2ffa85b6111f83fb6b427ca77eb905223807
bnegussie <bnegussie3@protonmail.com>
2020-12-29 16:36:29 -0800

    v0.1.2 created the RESTful API, for the Reminders, and making sure to filter by the current user.

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,20 +2,380 @@ const router = require("express").Router();
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
 
+// Getting all of the user's Reminders:
 router.get("/", authorization, async(req, res) => {
     try {
         // Now req.user has the payload:
-        const user = await pool.query("SELECT user_f_name FROM users WHERE user_id = $1;",
+        const user = await pool.query(
+            "SELECT u.user_id, all_r.reminder_id, all_r.reminder_completed, all_r.reminder_title, all_r.reminder_desc, all_r.reminder_due_date, all_r.reminder_reminder_date FROM users AS u LEFT JOIN all_reminders AS all_r ON u.user_id = all_r.user_id WHERE u.user_id = $1",
             [req.user]
         );
 
-        res.json(user.rows[0]);
+        res.json(user.rows);
 
     } catch (error) {
         console.error(error.message);
         res.status(500).json("Server error");
     }
 });
+
+/************************************** STARTS: all_reminders DB ******************************/
+// (Creating) Placing a new reminder into the all_reminders DB table:
+router.post("/reminder/all", authorization, async(req, res) => {
+    try {
+        const {completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const addingToAllReminders = await pool.query(
+            "INSERT INTO all_reminders (user_id, reminder_completed, reminder_title, reminder_desc, reminder_due_date, reminder_reminder_date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+            [req.user, completed, title, desc, dueDate, reminderDate]
+        );
+
+        res.status(200).json(addingToAllReminders.rows[0]);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get all reminders, filtering by the user cred:
+router.get("/reminder/all", authorization, async(req, res) => {
+    try {
+        const allReminders = await pool.query(
+            "SELECT * FROM all_reminders WHERE user_id = $1 ORDER BY reminder_due_date ASC, reminder_title ASC;", 
+            [req.user]
+        );
+
+        res.status(200).json(allReminders.rows);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get a specific reminder, filtering by the user cred:
+router.get("/reminder/all/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const getReminder = await pool.query(
+            "SELECT * FROM all_reminders WHERE reminder_id = $1 AND user_id = $2", [id, req.user]
+        );
+        if (getReminder.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to get this reminder.");
+        }
+        
+        res.status(200).json(getReminder.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Updating the reminder task:
+router.put("/reminder/all/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const updatingAllReminders = await pool.query(
+            "UPDATE all_reminders SET reminder_completed = $1, reminder_title = $2, reminder_desc = $3, reminder_due_date = $4, reminder_reminder_date = $5 WHERE reminder_id = $6 AND user_id = $7 RETURNING *",
+            [completed, title, desc, dueDate, reminderDate, id, req.user]
+        );
+        if (updatingAllReminders.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this update.");
+        }
+
+        res.status(200).json(updatingAllReminders.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Deleting the reminder from the all DB because the reminder has either 
+// been deleted.
+router.delete("/reminder/all/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deletingResponse = await pool.query(
+            "DELETE FROM all_reminders WHERE reminder_id = $1 AND user_id = $2 RETURNING *", [id, req.user]
+        );
+        if (deletingResponse.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this deletion.");
+        }
+
+        res.status(200).json("The reminder was successfully deleted.");
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+/************************************** ENDS: all_reminders DB ***********************************/
+/************************************** START: active_reminders DB ****************************/
+// (Creating) Placing a new reminder into the active_reminders DB table:
+router.post("/reminder/active", authorization, async(req, res) => {
+    try {
+        const {id, completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const addingToActiveReminders = await pool.query(
+            "INSERT INTO active_reminders (user_id, reminder_id, reminder_completed, reminder_title, reminder_desc, reminder_due_date, reminder_reminder_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+            [req.user, id, completed, title, desc, dueDate, reminderDate]
+        );
+
+        res.status(200).json(addingToActiveReminders.rows[0]);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get all active reminders, filtering by the user cred:
+router.get("/reminder/active", authorization, async(req, res) => {
+    try {
+        const allActiveReminders = await pool.query(
+            "SELECT * FROM active_reminders WHERE user_id = $1 ORDER BY reminder_due_date ASC, reminder_title ASC;",
+            [req.user]
+        );
+
+        res.status(200).json(allActiveReminders.rows);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get a specific reminder, filtering by the user cred:
+router.get("/reminder/active/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const getReminder = await pool.query(
+            "SELECT * FROM active_reminders WHERE reminder_id = $1 AND user_id = $2", [id, req.user]
+        );
+        if (getReminder.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to get this reminder.");
+        }
+        
+        res.status(200).json(getReminder.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Updating the active reminder task:
+router.put("/reminder/active/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const updatingActiveReminders = await pool.query(
+            "UPDATE active_reminders SET reminder_completed = $1, reminder_title = $2, reminder_desc = $3, reminder_due_date = $4, reminder_reminder_date = $5 WHERE EXISTS (SELECT reminder_id FROM active_reminders WHERE reminder_id = $6) AND reminder_id = $6 AND user_id = $7 RETURNING *",
+            [completed, title, desc, dueDate, reminderDate, id, req.user]
+        );
+        if (updatingActiveReminders.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this update.");
+        }
+
+        res.status(200).json("The active reminder was updated successfully.");
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Deleting the reminder from active DB because the reminder has either 
+// been completed or it has been deleted.
+router.delete("/reminder/active/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const delTodoResp = await pool.query(
+            "DELETE FROM active_reminders WHERE EXISTS (SELECT reminder_id FROM active_reminders WHERE reminder_id = $1) AND reminder_id = $1 AND user_id = $2 RETURNING *", 
+            [id, req.user]
+        );
+        if (delTodoResp.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this deletion.");
+        }
+
+        res.status(200).json(delTodoResp.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+/************************************** END: active_reminders DB *********************************/
+/************************************** START: completed_reminders DB ****************************/
+// Reminder task completed so it's being added to the completed list.
+router.post("/reminder/completed", authorization, async(req, res) => {
+    try {
+        const {id, completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const addingToCompletedReminders = await pool.query(
+            "INSERT INTO completed_reminders (user_id, reminder_id, reminder_completed, reminder_title, reminder_desc, reminder_due_date, reminder_reminder_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+            [req.user, id, completed, title, desc, dueDate, reminderDate]
+        );
+
+        res.status(200).json(addingToCompletedReminders.rows[0]);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get all completed reminders, filtering by the user cred:
+router.get("/reminder/completed", authorization, async(req, res) => {
+    try {
+        const allCompletedReminders = await pool.query(
+            "SELECT * FROM completed_reminders WHERE user_id = $1 ORDER BY reminder_due_date ASC, reminder_title ASC;",
+            [req.user]
+        );
+
+        res.status(200).json(allCompletedReminders.rows);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get a specific reminder, filtering by the user cred:
+router.get("/reminder/completed/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const getReminder = await pool.query(
+            "SELECT * FROM completed_reminders WHERE reminder_id = $1 AND user_id = $2", [id, req.user]
+        );
+        if (getReminder.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to get this reminder.");
+        }
+        res.status(200).json(getReminder.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Updating the completed reminder task:
+router.put("/reminder/completed/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const updatingCompletedReminders = await pool.query(
+            "UPDATE completed_reminders SET reminder_completed = $1, reminder_title = $2, reminder_desc = $3, reminder_due_date = $4, reminder_reminder_date = $5 WHERE EXISTS (SELECT reminder_id FROM completed_reminders WHERE reminder_id = $6) AND reminder_id = $6 AND user_id = $7 RETURNING *",
+            [completed, title, desc, dueDate, reminderDate, id, req.user]
+        );
+        if (updatingCompletedReminders.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this update.");
+        }
+
+        res.status(200).json("The completed reminder was updated successfully.");
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Deleting the reminder from completed DB because the reminder has either been
+// marked uncompleted or it has been deleted:
+router.delete("/reminder/completed/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deletingResponse = await pool.query(
+            "DELETE FROM completed_reminders WHERE EXISTS (SELECT reminder_id FROM completed_reminders WHERE reminder_id = $1) AND reminder_id = $1 AND user_id = $2 RETURNING *", 
+            [id, req.user]
+        );
+        if (deletingResponse.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this deletion.");
+        }
+
+        res.status(200).json(deletingResponse.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+/************************************** END: completed_reminders DB ******************************/
+/************************************** START: overdue_reminders DB ******************************/
+// Ading to the overdue reminder list.
+router.post("/reminder/overdue", authorization, async(req, res) => {
+    try {
+        const {id, completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const addingToOverdueReminders = await pool.query(
+            "INSERT INTO overdue_reminders (user_id, reminder_id, reminder_completed, reminder_title, reminder_desc, reminder_due_date, reminder_reminder_date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+            [req.user, id, completed, title, desc, dueDate, reminderDate]
+        );
+
+        res.status(200).json(addingToOverdueReminders.rows[0]);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get all overdue reminders, filtering by the user cred:
+router.get("/reminder/overdue", authorization, async(req, res) => {
+    try {
+        const allOverdueReminders = await pool.query(
+            "SELECT * FROM overdue_reminders WHERE user_id = $1 ORDER BY reminder_due_date ASC, reminder_title ASC;",
+            [req.user]
+        );
+
+        res.status(200).json(allOverdueReminders.rows);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Get a specific overdue reminder, filtering by the user cred:
+router.get("/reminder/overdue/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const getOverdueReminder = await pool.query(
+            "SELECT * FROM overdue_reminders WHERE reminder_id = $1 AND user_id = $2", [id, req.user]
+        );
+        if (getOverdueReminder.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to get this reminder.");
+        }
+        res.status(200).json(getOverdueReminder.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Updating the completed reminder task:
+router.put("/reminder/overdue/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {completed, title, desc, dueDate, reminderDate} = req.body;
+
+        const updatingOverdueReminder = await pool.query(
+            "UPDATE overdue_reminders SET reminder_completed = $1, reminder_title = $2, reminder_desc = $3, reminder_due_date = $4, reminder_reminder_date = $5 WHERE EXISTS (SELECT reminder_id FROM overdue_reminders WHERE reminder_id = $6) AND reminder_id = $6 AND user_id = $7 RETURNING *",
+            [completed, title, desc, dueDate, reminderDate, idm, req.user]
+        );
+        if (updatingOverdueReminder.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this update.");
+        }
+
+        res.status(200).json("The overdue reminder was updated successfully.");
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+// Deleting the reminder from overdue DB because the reminder has either gotten the due date
+// updated to an upcomming date or the reminder has completely been removed from this app:
+router.delete("/reminder/overdue/:id", authorization, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deletingResponse = await pool.query(
+            "DELETE FROM overdue_reminders WHERE EXISTS (SELECT reminder_id FROM overdue_reminders WHERE reminder_id = $1) AND reminder_id = $1 AND user_id = $2 RETURNING *", 
+            [id, req.user]
+        );
+        if (deletingResponse.rows.length === 0 ) {
+            return res.status(403).json("Unauthorized to perform this deletion.");
+        }
+
+        res.status(200).json(deletingResponse.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+/************************************** END: overdue_reminders DB ********************************/
 
 
 module.exports = router;
